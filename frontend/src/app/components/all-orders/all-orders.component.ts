d556552d0c50cc6ee8f31d0f890a35450a38f3db
Sophie Unterfranz <sophie.unterfranz@uni-konstanz.de>
2022-05-09 20:01:54 +0200

    add html code example

---
+++ frontend/src/app/components/all-orders/all-orders.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './all-orders.component.html',
+  styleUrls: ['./all-orders.component.css']
+})
+export class AllOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
