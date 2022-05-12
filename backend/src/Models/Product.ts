320dae7c22f8208aa1b05d0eac07fe6b20cafb72
Henri Grotzeck <grotzeck@opendress.com>
2022-05-12 20:44:18 +0200

    minor changes for test purposes

--- backend/src/Models/Product.ts
+++ backend/src/Models/Product.ts
@@ -13,19 +13,36 @@ interface IProduct extends Document{
     // Zeit dauer der Dienstleistung
     duration:Date
     // Möglichen daten wo man die Dienstleistung kaufen kann
-    timeslots:Date[]
+    appointments:IAppointment[]
 }
+
+interface IAppointment{
+    date:Date,
+    // gibt an ob es noch zu lesen der Termin noch angegeben wird
+    isReserved:boolean
+}
+
+const Appointment : Schema = new Schema<IAppointment>(
+{
+    date:{type:Date,required:true},
+    // gibt an ob es noch zu lesen der Termin noch angegeben wird
+    isReserved:{type:Boolean,required:true}
+});
+
+
 // create the Schema of IProduct
 const productSchema : Schema = new Schema<IProduct>({
-  name:       { type: String, required: true },
-  user:       { type: String, required: true },
-  description:{ type: String, required: true },
-  prize:      { type: Number, required: true },
-  duration:   { type: Date,   required: true },
-  timeslots:  { type: [Date], required: true }
+  name:          { type: String, required: true },
+  user:          { type: String, required: true },
+  description:   { type: String, required: true },
+  prize:         { type: Number, required: true },
+  duration:      { type: Date,   required: true },
+  appointments:  { type: [Appointment], required: true }
 });
+
+
 
 // 3. Create a Model.
 const Product : Model<IProduct>  = model<IProduct>('Product', productSchema);
 
-export {IProduct,Product};
+export {IProduct,Product,IAppointment};
