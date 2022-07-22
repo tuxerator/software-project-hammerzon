8e187e30ee6286e048aafa5af8ca59efef6dcbec
Henri Grotzeck <grotzeck@opendress.com>
2022-07-22 13:51:28 +0200

    reorganized project directory

--- frontend/src/util/util.ts
+++ frontend/src/util/util.ts
@@ -5,17 +5,17 @@ import { FormGroup } from '@angular/forms';
 export const utcOffset: number = new Date(0).getTime();
 export const dayInMilliseconds: number = 24 * 60 * 60 * 1000;
 
-export function isInteger(value: any): value is number {
+export const  isInteger = (value: any): value is number => {
   return typeof value === 'number' && isFinite(value) && Math.floor(value) === value;
-}
+};
 
 export const ngbDateToDate = (ngbDate: NgbDate | null): Date | null => {
   return ngbDate ? new Date(Date.UTC(ngbDate.year, ngbDate.month - 1, ngbDate.day)) : null;
-}
+};
 
 export const getDayTime = (date: Date): number => {
   return date.getUTCHours() * 60 * 60 * 1000 + date.getUTCMinutes() * 60 * 1000 + date.getUTCSeconds() * 1000 + date.getUTCMilliseconds();
-}
+};
 
 
 /**
@@ -25,7 +25,7 @@ export const getDayTime = (date: Date): number => {
  */
 export const addTimezoneOffset = (date: Date): Date => {
   return new Date(date.getTime() + utcOffset);
-}
+};
 
 /**
  * Compares two dates without taking the time into account.
@@ -35,7 +35,7 @@ export const compareDates = (date1: Date, date2: Date): number => {
   const date1WithoutTime = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
   const date2WithoutTime = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
   return Math.sign(date1WithoutTime - date2WithoutTime);
-}
+};
 
 export const createAppointment = (startDate: Date, duration: number, defaultTimeFrame: Availability): Availability => {
   console.log('startDate: %o, duration: %o, defaultTimeFrame: %o', startDate, duration, defaultTimeFrame);
@@ -51,11 +51,11 @@ export const createAppointment = (startDate: Date, duration: number, defaultTime
   endDate.setTime(endDate.getTime() + duration);
   console.log('endDate: %o', endDate);
   return new Availability(startDate, endDate);
-}
+};
 
 // update the validity of the controls in the group
 export const updateGroupValidity = (formGroup: FormGroup): void => {
   console.log('update validity for: %o', formGroup);
   Object.keys(formGroup.controls).forEach(key => formGroup.controls[key].updateValueAndValidity());
-  console.log(`Updated Validity for `, formGroup.controls);
-}
+  console.log('Updated Validity for ', formGroup.controls);
+};
