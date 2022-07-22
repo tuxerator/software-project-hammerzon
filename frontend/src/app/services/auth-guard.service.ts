4f92d2270e59f5249e9f589fb578fd83fbad2b56
Jakob Sanowski <jakob.sanowski@uni-konstanz.de>
2022-07-22 14:57:49 +0200

    feat: continue previous route after login

--- frontend/src/app/services/auth-guard.service.ts
+++ frontend/src/app/services/auth-guard.service.ts
@@ -11,22 +11,24 @@ export class AuthGuardService implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {
   }
 
-  async canActivate(): Promise<boolean> {
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     //console.log(this.authService);
     const isLoggedIn = this.authService.isLogedIn();
-   // console.log(isLoggedIn);
+    // console.log(isLoggedIn);
     if (isLoggedIn) {
       return true;
     } else {
       //this.router.navigate(['/login']);
       // Check whether User is Logedin by requesting it from the server
       console.log('asked Server');
-      const user = await firstValueFrom(this.authService.getUser());
-      //console.log(user);
-      if(user){
+      const user = await firstValueFrom(this.authService.getUser()).catch(err => console.log('error: ' + err));
+      console.log('user: ', user);
+      if (user) {
         //console.log(user);
         return true;
       }
+      console.log('User not logged in. Redirecting to login.');
+      this.router.navigate(['login'], { queryParams: { returnUrl: state.url } });
       return false;
     }
   }
